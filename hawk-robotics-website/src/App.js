ff9dcadb34e6a31cdeeaa7dc7dbaf6891f5e2084
colinteahan <24cteahan@xbhs.net>
2022-03-11 13:18:38 -0500

    update

--- hawk-robotics-website/src/App.js
+++ hawk-robotics-website/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>Hawk Robotics: The Ryken Force</p>
-      <div id='paragraph'>   Established in 2013, Hawk Robotics is a competitive team 
+      <div id='paragraph'>   Established in 2021, Hawk Robotics is a competitive team 
         available for every student interested in the fields of 
         coding and engineering. Students will participate in programming 
         and constructing a robot during the competitive season. Afterwards students learn more programming while beginning to 
