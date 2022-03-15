26ea46956fb402dbfe08a0d50cfacee9ef234931
colinteahan <24cteahan@xbhs.net>
2022-03-15 14:42:25 -0400

    update

---
+++ hawk-robotics-website/public/public-index.js
@@ -0,0 +1,12 @@
+document.onload = function() {
+    // When the user scrolls down 20px from the top of the document, slide down the navbar
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("menu_bar").style.top = "0";
+    } else {
+        document.getElementById("menu_bar").style.top = "-50px";
+    }
+    }
+}
